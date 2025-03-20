034fcece027624af2467388ea55b2243d37fabd6
Swetha004 <126387165+Swetha004@users.noreply.github.com>
2025-03-20 12:00:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
+    docker login -u swethar22cse -p dckr_pat_aery1fdAaQbVfyWwJsAx510VN6A
     docker tag test ar8888/dev
     docker push ar8888/dev
-    docker-compose up -d
     
