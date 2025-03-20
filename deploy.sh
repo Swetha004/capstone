139b0f06649dcd19478735ba62b1978a69d703ac
Swetha004 <126387165+Swetha004@users.noreply.github.com>
2025-03-20 13:34:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u swethar22cse -p dckr_pat_aery1fdAaQbVfyWwJsAx510VN6A
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker tag test1 swethar22cse/dev
+    docker push swethar22cse/dev
     
